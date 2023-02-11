159c1660dd567345dbaed4bc7dd6c83810355250
Luis Carlos <cienfuegos15givi@gmail.com>
2023-02-11 10:17:56 -0600

    primer commit

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+// tiene la configuracion general de firebase
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdF8w7juMbW8IzoYnSD9gt3SKPd9laXVA",
+  authDomain: "sdp-expo-rn.firebaseapp.com",
+  projectId: "sdp-expo-rn",
+  storageBucket: "sdp-expo-rn.appspot.com",
+  messagingSenderId: "647088028380",
+  appId: "1:647088028380:web:03bf5d776dc5871acd9219",
+  measurementId: "G-597P9HPRVE",
+  databaseURL: "https://sdp-expo-rn-default-rtdb.firebaseio.com/",
+};
+//databaseURL  apunta a la base de datos
+// inicia la app con la configuracion de firebase
+const app = initializeApp(firebaseConfig);
+
+export { app, getDatabase, ref, set };
